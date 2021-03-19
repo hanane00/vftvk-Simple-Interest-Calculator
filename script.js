b3b406a62062561035873398bbcfad996fca3a48
hanane00 <80666798+hanane00@users.noreply.github.com>
2021-03-19 01:35:14 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
+function handleChange() {
+    let rate = document.getElementById("rate").value;    
+    document.getElementById("interest").innerHTML = rate + "%";
+}
+
 function compute()
 {
-    p = document.getElementById("principal").value;
+    let principal = document.getElementById("principal").value;
+    let rate = document.getElementById("rate").value;
+    let years = document.getElementById("years").value;
+    let interest = principal * years * rate / 100;    
     
+    let date = new Date().getFullYear();
+    let newDate = date + parseInt(years);
+    
+    if (principal <= 0 ) {
+        alert('Enter a positive number')
+    } else {
+
+    document.getElementById("result").innerHTML = 
+        "If you deposit" + ' ' + principal + "," + "</br>" 
+        + "at an interest rate of" + ' ' + rate + "%." + "</br>"
+        + "You will receive an amount of" + ' ' + interest + "," + "</br>"
+        + "in the year" + ' ' + newDate
+    }
 }
-        
